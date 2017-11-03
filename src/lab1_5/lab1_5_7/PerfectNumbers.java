5838c49bef2c04fb4a0e5ebf53a05486e5a30dde
Roman Peresada <roman.peresada@nure.ua>
2017-11-03 15:13:10 +0200

    made labs 1_5

---
+++ src/lab1_5/lab1_5_7/PerfectNumbers.java
@@ -0,0 +1,24 @@
+package lab1_5.lab1_5_7;
+
+import java.util.Scanner;
+
+public class PerfectNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int x = Integer.parseInt(sc.nextLine());
+        int result;
+        for (int i = 1; i <= x; i++) {
+            result = 0;
+            for (int j = 1; j <= i / 2; j++) {
+                if (i % j == 0)
+                    result += j;
+            }
+            if (result == i)
+                System.out.println(i);
+
+
+        }
+
+    }
+}
