fe5e317594dcea8393de0cac3c6cc1acc7867afc
Roman Peresada <roman.peresada@nure.ua>
2017-11-03 13:54:09 +0200

    made 1-2

---
+++ src/lab1_5/lab1_5_2/Number.java
@@ -0,0 +1,116 @@
+package lab1_5.lab1_5_2;
+
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("If you want do Switch - Enter \"s\"");
+        System.out.println("If you want do IF-ELSE - Enter \"i\"");
+
+        String s =sc.nextLine();
+
+        if(s.equals("s")) {
+            System.out.println("Enter number: ");
+            int x = Integer.parseInt(sc.nextLine());
+            switch (x) {
+                case 1:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Nine");
+                    break;
+                default:
+                    System.out.println("Enter number - "+x);
+                    System.out.println("Other");
+                    break;
+
+            }
+        }
+        else if(s.equals("i"))
+        {
+            System.out.println("Enter number: ");
+            int x = Integer.parseInt(sc.nextLine());
+            if(x==1)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("One");
+            }
+            else if(x==2)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Two");
+            }
+            else if(x==3)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Three");
+            }
+            else if(x==4)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Four");
+            }
+            else if(x==5)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Five");
+            }
+            else if(x==6)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Six");
+            }
+            else if(x==7)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Seven");
+            }
+            else if(x==8)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Eight");
+            }
+            else if(x==9)
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Nine");
+            }
+            else
+            {
+                System.out.println("Enter number - "+x);
+                System.out.println("Other");
+            }
+        }
+    }
+}
