686d866646b60c16d2c8b1709514b982e7fbcac2
Roman Peresada <roman.peresada@nure.ua>
2017-11-16 16:29:15 +0200

    finished lab 2_7_1

---
+++ src/module2/lab2_7/Monitor.java
@@ -0,0 +1,31 @@
+package module2.lab2_7;
+
+public class Monitor extends Device {
+    private int resolutionX;
+    private int resolutionY;
+
+    public int getResolutionX() {
+        return resolutionX;
+    }
+
+    public void setResolutionX(int resolutionX) {
+        this.resolutionX = resolutionX;
+    }
+
+    public int getResolutionY() {
+        return resolutionY;
+    }
+
+    public void setResolutionY(int resolutionY) {
+        this.resolutionY = resolutionY;
+    }
+
+    public Monitor(String manufacturer, float price, String serialNumber, int resolutionX, int resolutionY) {
+        super(manufacturer, price, serialNumber);
+        this.resolutionX = resolutionX;
+        this.resolutionY = resolutionY;
+    }
+
+
+
+}
