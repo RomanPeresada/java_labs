47e6972a434dd48106ca2cd1ed63546740531bf3
Roman Peresada <roman.peresada@nure.ua>
2017-11-26 15:25:33 +0200

    question about task 3

--- src/module2/lab10/lab3/Main.java
+++ src/module2/lab10/lab3/Main.java
@@ -1,7 +1,7 @@
 package module2.lab10.lab3;
 
-
 import module2.lab2_8.com.brainacad.oop.testshapes.Circle;
+import module2.lab2_8.com.brainacad.oop.testshapes.Rectangle;
 import module2.lab2_8.com.brainacad.oop.testshapes.Triangle;
 
 public class Main {
@@ -9,6 +9,15 @@ public class Main {
         String[] arrayOfStrings = {"Rectangle:RED:10,20",
                                     "Triangle:GREEN:9,7,12",
                                     "Circle:BLACK:10"};
+
+        Rectangle rectangle = (Rectangle)Shape.parseShape(arrayOfStrings[0]);
+        System.out.println(rectangle);
+
+        Triangle triangle = (Triangle)Shape.parseShape(arrayOfStrings[1]);
+        System.out.println(triangle);
+
+        Circle circle = (Circle)Shape.parseShape(arrayOfStrings[2]);
+        System.out.println(circle);
 
         Shape[] shapes = new Shape[3];
         shapes[0] = (Shape)Shape.parseShape(arrayOfStrings[0]);
